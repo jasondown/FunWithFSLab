4dc8f1216780ca45d7f7b533afcee1f50e9e15a6
Jason Down <jdown@igloosoftware.com>
2018-01-12 00:39:35 -0500

    Playing with deedle frames.

--- DeedleExmpale.fsx
+++ DeedleExmpale.fsx
@@ -20,4 +20,38 @@ let getData year indicator =
         defaultArg (Option.map float value) nan
     series [ for d in xml.Datas -> d.Country.Value, orNaN d.Value ]
 
- 
+let wb = WorldBankData.GetDataContext()
+let inds = wb.Countries.World.Indicators
+let code = inds.``CO2 emissions (kt)``.IndicatorCode
+
+let co2000 = getData 2000 code
+let co2010 = getData 2010 code
+
+// Plot the data for years 2000 and 2010
+Chart.Geo(co2000)
+Chart.Geo(co2010)
+
+// Calculate the change (as %) over the last 10 years
+let change = (co2010 - co2000) / co2000 * 100.0
+
+// Plot the change between 2000 and 2010 and
+// configure chart options (colors) to make it nicer
+let cs = "#77D53D;#D1C855;#E8A958;#EA4C41;#930700"
+let vs = [| -10;0;100;200;1000 |]
+let axis = ColorAxis(values=vs, colors=cs.Split(';'))
+
+Chart.Geo(change)
+|> Chart.WithOptions(Options(colorAxis=axis))
+
+let codes =
+    [ "CO2", inds.``CO2 emissions (metric tons per capita)``
+      "Uni-F", inds.``School enrollment, tertiary, female (% gross)``
+      "Uni-M", inds.``School enrollment, tertiary, male (% gross)``
+      "Life", inds.``Life expectancy at birth, total (years)``
+      "Growth", inds.``GDP per capita growth (annual %)``
+      "Pop", inds.``Population growth (annual %)``
+      "GDP", inds.``GDP per capita (current US$)`` ]
+
+let world = 
+  frame [ for name, ind in codes -> 
+            name, getData 2010 ind.IndicatorCode ]

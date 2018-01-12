7d1e75317babd82a3e479b8332e5a4c5edd4b84f
Jason Down <jdown@igloosoftware.com>
2018-01-12 00:10:54 -0500

    Grabbing GDP indicators for countries.

--- DeedleExmpale.fsx
+++ DeedleExmpale.fsx
@@ -1,9 +1,23 @@
 ﻿#r "System.Xml.Linq.dll"
-#load "packages/FsLab/FsLab.fsx"
+#load "./packages/FsLab/FsLab.fsx"
 
 open Deedle
 open FSharp.Data
 open XPlot.GoogleCharts
 open XPlot.GoogleCharts.Deedle
 
+type WorldData = XmlProvider<"http://api.worldbank.org/countries/indicators/NY.GDP.PCAP.CD?date=2010:2010">
 
+let indUrl = "http://api.worldbank.org/countries/indicators/"
+
+let getData year indicator =
+    let query =
+        [ ("per_page","1000"); 
+        ("date",sprintf "%d:%d" year year) ]
+    let data = Http.RequestString(indUrl + indicator, query)
+    let xml = WorldData.Parse(data)
+    let orNaN value = 
+        defaultArg (Option.map float value) nan
+    series [ for d in xml.Datas -> d.Country.Value, orNaN d.Value ]
+
+ 
